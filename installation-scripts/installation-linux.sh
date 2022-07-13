1b4d51017331f91aa47ab54f9a85fe5561fccf0a
nikita.demakov <your_email_address@example.com>
2022-07-13 19:39:26 +0700

    fix: edit color schema

--- installation-scripts/installation-linux.sh
+++ installation-scripts/installation-linux.sh
@@ -9,8 +9,8 @@ echo "Checked OS: $OS_NAME $OS_VERSION"
 echo "Starting Docker installation script for $OS_NAME $OS_VERSION"
 
 if [[ $OS_NAME -eq "Ubuntu" ]]; then
+    echo "Starting installation of Docker"
     if [[ $OS_VERSION == "22.04" ]]; then
-      echo "Starting installation of Docker"
       sudo apt update
       sudo apt install apt-transport-https ca-certificates curl software-properties-common
       curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -18,7 +18,6 @@ if [[ $OS_NAME -eq "Ubuntu" ]]; then
       sudo apt update
       apt-cache policy docker-ce
       sudo apt install docker-ce
-      echo "Docker installed successfully"
     fi
 
     if [[ $OS_VERSION == "20.04" ]]; then
@@ -30,12 +29,12 @@ if [[ $OS_NAME -eq "Ubuntu" ]]; then
       apt-cache policy docker-ce
       sudo sudo apt install docker-ce
     fi
+    echo -e "You have installed: \033[43m\033[30m$(docker --version)\033[0m"
 
     echo "Starting installation of Docker compose"
     sudo curl -SL "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
-    docker-compose --version
-    echo "Docker compose installed successfully"
+    echo -e "You have installed: \033[43m\033[30m$(docker-compose --version)\033[0m"
     #Добавляем пользователя в группу docker
     sudo usermod -aG docker ${USER}
 fi
