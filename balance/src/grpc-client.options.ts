67664eb1347efaf471d17d68c1c4728f4acac29c
Jo <66825013+Usaldudo@users.noreply.github.com>
2022-07-20 17:12:08 +0600

    feat: Create service in docker-compose

--- balance/src/grpc-client.options.ts
+++ balance/src/grpc-client.options.ts
@@ -4,7 +4,7 @@ import { join } from 'path';
 export const grpcClientOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
-    url: '127.0.0.1:4002',
+    url: '0.0.0.0:4002',
     package: 'hello',
     protoPath: join(__dirname, './hello/hello.proto'),
   },
