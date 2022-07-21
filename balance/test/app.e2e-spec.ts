ff23c4d96ecd3052918002d966d735724bd484cb
dnzvsh <zuevdanil@gmail.com>
2022-07-21 16:21:02 +0700

    fix: make real imports for test file

--- balance/test/app.e2e-spec.ts
+++ balance/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { BalanceModule } from './../src/app.module';
+import { BalanceModule } from '../src/balance.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
