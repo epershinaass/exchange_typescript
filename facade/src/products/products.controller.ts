df48466a1dfc71e5568d789daec1058194b6f66d
dnzvsh <zuevdanil@gmail.com>
2022-07-27 18:19:54 +0700

    fix: connect facade and product service

--- facade/src/products/products.controller.ts
+++ facade/src/products/products.controller.ts
@@ -1,5 +1,11 @@
 import { Body, Controller, OnModuleInit } from '@nestjs/common';
-import { Client, ClientGrpc, ClientOptions, GrpcMethod, Transport } from '@nestjs/microservices';
+import {
+  Client,
+  ClientGrpc,
+  ClientOptions,
+  GrpcMethod,
+  Transport,
+} from '@nestjs/microservices';
 import { join } from 'path';
 import { AddProductsDto, GetProductDto } from './dto/add-products.dto';
 import { IGrpcService } from './interfaces/grpc.interface';
@@ -7,6 +13,7 @@ import { IGrpcService } from './interfaces/grpc.interface';
 export const microserviceOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
+    url: '0.0.0.0:5001',
     package: 'products',
     protoPath: join(__dirname, './proto/products.proto'),
   },
@@ -19,15 +26,16 @@ export class ProductsController implements OnModuleInit {
   private grpcService: IGrpcService;
 
   onModuleInit() {
-    this.grpcService = this.client.getService<IGrpcService>('ProductsController');
+    this.grpcService =
+      this.client.getService<IGrpcService>('ProductsController');
   }
 
-  @GrpcMethod('ProductController', 'AddProduct')
+  @GrpcMethod('ProductsController', 'AddProduct')
   addProduct(@Body() addProductsDto: AddProductsDto) {
     return this.grpcService.addProduct(addProductsDto);
   }
 
-  @GrpcMethod('ProductController', 'GetProducts')
+  @GrpcMethod('ProductsController', 'GetProducts')
   getProducts(@Body() getProductDto: GetProductDto) {
     return this.grpcService.getProducts(getProductDto);
   }
