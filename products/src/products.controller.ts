f3da1b01879036f18c3628b27fcd61000f87b215
dnzvsh <zuevdanil@gmail.com>
2022-07-27 18:20:19 +0700

    refactor: some spaces

--- products/src/products.controller.ts
+++ products/src/products.controller.ts
@@ -5,7 +5,7 @@ import { IAddProductRequest, IUserId } from './interfaces/object.interface';
 
 @Controller()
 export class ProductsController {
-  constructor(private productsService: ProductsService) {}
+  constructor(private productsService: ProductsService) { }
 
   @GrpcMethod('ProductsController', 'AddProduct')
   async addProduct(addProductRequest: IAddProductRequest): Promise<string> {
@@ -14,6 +14,6 @@ export class ProductsController {
   @GrpcMethod('ProductsController', 'GetProducts')
   async getProducts(userId: IUserId) {
     const UserProducts = await this.productsService.getProducts(userId);
-    return {products: UserProducts.products};
+    return { products: UserProducts.products };
   }
 }
