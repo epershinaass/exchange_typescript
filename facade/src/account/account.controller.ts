206ba5de16c11ffa3e543c3172576c27cfb56d19
usr <kirill.moskva@mail.ru>
2022-08-01 12:29:12 +0600

    draft

--- facade/src/account/account.controller.ts
+++ facade/src/account/account.controller.ts
@@ -11,23 +11,25 @@ import { join } from 'path';
 import { firstValueFrom, lastValueFrom, withLatestFrom } from 'rxjs';
 import { CredentialsDto, AuthMessageDto, MessageDto } from './dto/account-dto';
 import { IAccountGrpcService } from './interface/account-grpc-interface';
+import { ConfigService } from '@nestjs/config';
+const crypto = require('crypto');
 
 export const microserviceOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
     package: 'account',
-    url: `${process.env.ACCOUNT_URL??'0.0.0.0'}:${process.env.ACCOUNT_PORT??'5002'}`,
+    url: `${'0.0.0.0'}:${'5002'}`, // TODO: pass configService here
     protoPath: join(__dirname, './proto/account-grpc.proto'),
   },
 };
 
 @Controller()
 export class AccountController implements OnModuleInit {
+  constructor(private configService: ConfigService) {}
   @Client(microserviceOptions)
   private client: ClientGrpc;
-
   private grpcService: IAccountGrpcService;
-
+  private salt: string = this.configService.get('ACC_PASSW_SALT');
   onModuleInit() {
     this.grpcService =
       this.client.getService<IAccountGrpcService>('AccountController');
@@ -35,6 +37,7 @@ export class AccountController implements OnModuleInit {
 
   @GrpcMethod('AccountController', 'SignIn')
   async signIn(@Body() creds: CredentialsDto): Promise<AuthMessageDto> {
+    creds.password = crypto.pbkdf2Sync(creds.password, this.salt, 1, 64, 'sha512').toString();
     try {
       return await firstValueFrom(this.grpcService.signIn(creds))
     } catch (err) {
@@ -44,6 +47,7 @@ export class AccountController implements OnModuleInit {
 
   @GrpcMethod('AccountController', 'SignUp')
   async signUp(@Body() creds: CredentialsDto): Promise<MessageDto> {
+    creds.password = crypto.pbkdf2Sync(creds.password, this.salt, 1, 64, 'sha512').toString();
     try {
       return await firstValueFrom(this.grpcService.signUp(creds))
     } catch (err) {
--- facade/src/app.module.ts
+++ facade/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BalanceController } from './balance/balance.controller';
 import { AccountController } from './account/account.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [BalanceController, AccountController],
   providers: [],
 })
