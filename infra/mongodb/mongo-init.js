687c5596be73b09be211d0786892cc68bd1d6c54
Demakov Nikita <demakoffnik@gmail.com>
2022-08-02 17:01:56 +0700

    perf: make script simple

--- infra/mongodb/mongo-init.js
+++ infra/mongodb/mongo-init.js
@@ -4,9 +4,8 @@ db.createCollection('balances');
 
 db.balances.insertMany([
     {
-        _id: "62e36f4a65eec4910c2ba2e0",
         total: 2150,
-        userId: "62e370f465eec4910c2ba2e1",
+        userId: ObjectId("62e370f465eec4910c2ba2e1"),
         transactions: [
             {
                 transactionId: "ipsum in do",
@@ -16,7 +15,6 @@ db.balances.insertMany([
                         $numberLong: 1659072648876
                     }
                 },
-                _id: "62e3708890675efdf716f5a8"
 
             },
             {
@@ -27,7 +25,6 @@ db.balances.insertMany([
                         $numberLong: 1659075273087
                     }
                 },
-                _id: "62e37ac990675efdf716f5ad"
 
             },
             {
@@ -38,7 +35,6 @@ db.balances.insertMany([
                         $numberLong: "1659088480057"
                     }
                 },
-                _id: "62e3ae6090675efdf716f5bd"
 
             },
             {
@@ -49,7 +45,6 @@ db.balances.insertMany([
                         $numberLong: 1659325183184
                     }
                 },
-                _id: "62e74aff5f05e26a7a3c5929"
             }
 
         ]
