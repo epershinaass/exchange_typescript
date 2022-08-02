dd51b82d172a9d26e2f2e87d16fdbd0acdc3ba1d
Demakov Nikita <demakoffnik@gmail.com>
2022-08-02 09:24:49 +0700

    perf: remove init document

--- infra/mongodb/mongo-init.sh
+++ infra/mongodb/mongo-init.sh
@@ -21,62 +21,7 @@ mongo_Balance () {
     }
   )
 EOF
-  mongo -u $SERVICE_USER -p $SERVICE_PASSWORD --authenticationDatabase $SERVICE_DB<< EOF
-  use $SERVICE_DB;
 
-  db.balances.insert({
-                       "_id": ObjectId("62e36f4a65eec4910c2ba2e0"),
-                       "total": 2150,
-                       "userId": ObjectId("62e370f465eec4910c2ba2e1"),
-                       "transactions": [
-                         {
-                           "transactionId": "ipsum in do",
-                           "refillSum": 100,
-                           "transactionTime": {
-                             "$date": {
-                               "$numberLong": "1659072648876"
-                             }
-                           },
-                           "_id": ObjectId("62e3708890675efdf716f5a8")
-
-                         },
-                         {
-                           "transactionId": "qwe123",
-                           "refillSum": 25,
-                           "transactionTime": {
-                             "$date": {
-                               "$numberLong": "1659075273087"
-                             }
-                           },
-                           "_id": ObjectId("62e37ac990675efdf716f5ad")
-
-                         },
-                         {
-                           "transactionId": "tempor ullamco laboris laborum culpa",
-                           "refillSum": 2000,
-                           "transactionTime": {
-                             "$date": {
-                               "$numberLong": "1659088480057"
-                             }
-                           },
-                           "_id": {
-                             "$oid": ObjectId("62e3ae6090675efdf716f5bd")
-                           }
-                         },
-                         {
-                           "transactionId": "magna ipsum",
-                           "refillSum": 25,
-                           "transactionTime": {
-                             "$date": {
-                               "$numberLong": "1659325183184"
-                             }
-                           },
-                           "_id": ObjectId("62e74aff5f05e26a7a3c5929")
-                           }
-
-                       ]
-                     });
-EOF
 }
 
 mongo_Auth () {
