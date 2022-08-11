5a8f63f92d9e184dfd824f1e74b96d57b3a02c16
Jo <66825013+Usaldudo@users.noreply.github.com>
2022-08-11 16:29:54 +0600

    feat: add account db init

--- infra/mongodb/mongosetup.sh
+++ infra/mongodb/mongosetup.sh
@@ -79,6 +79,45 @@ quit();
 EOF
 }
 
+account () {
+mongosh --host "$SERVICE_DB_URL":27017 -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" <<EOF
+db = db.getSiblingDB('$SERVICE_DB');
+db.$SERVICE_DB.insertMany([
+  {
+    "userId": "1",
+    "products": [
+      {
+        "name": "init",
+        "quantity": 1
+      }
+    ]
+  }
+]);
+db.createUser({
+  user: '$SERVICE_USER',
+  pwd: '$SERVICE_PASSWORD',
+  roles: [
+    {
+      role: 'readWrite',
+      db: '$SERVICE_DB',
+    },
+  ],
+});
+use admin;
+db.createUser(
+  {
+    user: 'mongodb_exporter',
+    pwd: '$MONGODB_EXPORTER_PASSWORD',
+    roles: [
+      { role: 'clusterMonitor', db: 'admin' },
+      { role: 'read', db: 'local' }
+    ]
+  }
+);
+quit();
+EOF
+}
+
 echo "**********************************************" "$SERVICE_DB_URL"
 
 echo SETUP.sh time now: "$(date +"%T")"
@@ -86,4 +125,6 @@ if [[ "$SERVICE_DB" == "balance" ]]; then
   balance
 elif [[ "$SERVICE_DB" == "products" ]]; then
   products
+elif [[ "$SERVICE_DB" == "account" ]]; then
+  account
 fi
