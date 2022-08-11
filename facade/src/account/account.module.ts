f347b45c1c7db2c392dee1323a0eca4cb0b8825f
dnzvsh <zuevdanil@gmail.com>
2022-08-11 21:31:59 +0700

    fix: it works

--- facade/src/account/account.module.ts
+++ facade/src/account/account.module.ts
@@ -5,7 +5,6 @@ import { Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-
 @Module({
   imports: [
     // ConfigModule.forRoot({
@@ -14,18 +13,19 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     // ConfigModule,
     ClientsModule.registerAsync([
       {
-        inject:[ConfigService],
-        useFactory: (config: ConfigService) => {}
         name: 'ACCOUNT_GRPC_SERVICE',
-        transport: Transport.GRPC,
-        options: {
-          package: 'account',
-          url: `${process.env.ACCOUNT_URL}:${process.env.ACCOUNT_PORT}`,
-          protoPath: join(__dirname, './proto/account.proto'),
-        },
-      }
+        inject: [ConfigService],
+        useFactory: async (config: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            package: 'account',
+            url: `${process.env.ACCOUNT_URL}:${process.env.ACCOUNT_PORT}`,
+            protoPath: join(__dirname, './proto/account.proto'),
+          },
+        }),
+      },
     ]),
   ],
   controllers: [AccountController],
 })
-export class AccountModule { }
+export class AccountModule {}
