3d27b9582d8b1aa82cdd2dd250c3a7c1c727b4a3
konivan <konivan08@gmail.com>
2022-08-12 17:13:41 +0300

    change modules

--- products/src/products.module.ts
+++ products/src/products.module.ts
@@ -6,14 +6,17 @@ import { Product, ProductSchema } from './schemas/product.schema';
 import { ConfigModule } from '@nestjs/config';
 import { Catalog, CatalogSchema } from './schemas/productsCollection.schema';
 
+const db_path = () => {
+  const user = process.env.DB_USER!=='' ?`${process.env.DB_USER}:${process.env.DB_PASSWORD}@` : '';
+  return `mongodb://${user}${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+}
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    MongooseModule.forRoot(
-      `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-    ),
+    MongooseModule.forRoot((console.log(db_path()), db_path())),
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }, { name: Catalog.name, schema: CatalogSchema }]),
   ],
   controllers: [ProductsController],
