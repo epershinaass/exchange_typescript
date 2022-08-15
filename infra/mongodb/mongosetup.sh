3378a066178f4c8bf76084c13cb60fb62b464c8a
Jo <66825013+Usaldudo@users.noreply.github.com>
2022-08-15 19:49:28 +0600

    ci: add order deploy

--- infra/mongodb/mongosetup.sh
+++ infra/mongodb/mongosetup.sh
@@ -116,6 +116,34 @@ quit();
 EOF
 }
 
+order () {
+mongosh --host "$SERVICE_DB_URL":27017 -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" <<EOF
+db = db.getSiblingDB('$SERVICE_DB');
+db.createUser({
+  user: '$SERVICE_USER',
+  pwd: '$SERVICE_PASSWORD',
+  roles: [
+    {
+      role: 'readWrite',
+      db: '$SERVICE_DB',
+    },
+  ],
+});
+use admin;
+db.createUser(
+  {
+    user: 'mongodb_exporter',
+    pwd: '$MONGODB_EXPORTER_PASSWORD',
+    roles: [
+      { role: 'clusterMonitor', db: 'admin' },
+      { role: 'read', db: 'local' }
+    ]
+  }
+);
+quit();
+EOF
+}
+
 echo "**********************************************" "$SERVICE_DB_URL"
 
 echo SETUP.sh time now: "$(date +"%T")"
@@ -125,4 +153,6 @@ elif [[ "$SERVICE_DB" == "products" ]]; then
   products
 elif [[ "$SERVICE_DB" == "account" ]]; then
   account
+elif [[ "$SERVICE_DB" == "order" ]]; then
+  order
 fi
